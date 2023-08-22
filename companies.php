444e906afbc6b1bed67dca72c0d103a8766bff67
yamaguchi-ky <87267471+yamaguchi-ky@users.noreply.github.com>
2023-08-22 21:14:31 +0900

    Update companies.php

--- companies.php
+++ companies.php
@@ -3,16 +3,23 @@
 namespace App\models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\testUsers;
 
 class companies extends Model
 {
     protected $table = 'companies';
 
     protected $fillable = [
+        'id',
         'company_name',
         'street_adress',
         'representative_name'
     ];
+
+    public function testUsers(){
+        return $this->hasMany(testUsers::class,'company_id');
+    } 
+
 
 
 }
